26fa2d9cdcf60fec6df16a2791d5cda6d41d5a67
rohan37-pro <rohan2002maji@gmail.com>
2023-04-03 15:41:01 +0530

    made a little change, but you always remember that little changes can play a very significant role in life. dunno why i am writing all this, ooh i'm so damn alone.

--- 3_g_countVowels.py
+++ 3_g_countVowels.py
@@ -1,16 +1,18 @@
-# 
+# WAP TO COUNT THE number of vowels in the pieapple
 
 s = input("enter : ")
 
 vowels = "aeiou"
 vow_dict = {}
+count = 0
 for i in s:
     if i in vowels:
+        count+= 1
         if i in vow_dict:
             vow_dict[i] += 1
         else :
-            vow_dict[i] = 0
+            vow_dict[i] = 1
 
-print("vowels count in string --> ")
+print(f"total vowels count {count} --> ")
 for i in vow_dict:
     print(f"vowel {i} : {vow_dict[i]}")
