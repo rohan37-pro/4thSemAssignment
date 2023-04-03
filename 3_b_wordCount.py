b54d32f71f97bb8dd6fd3e1fe55e6f75e8ef603e
rohan37-pro <rohan2002maji@gmail.com>
2023-04-03 14:55:25 +0530

    word count

--- 3_b_wordCount.py
+++ 3_b_wordCount.py
@@ -1,1 +1,6 @@
 # WAP that reads a line, then count words and display how many words are there in the line.
+
+s = input("enter : ")
+
+s_list = s.strip().split(" ")
+print("word count --> ", len(s_list))
