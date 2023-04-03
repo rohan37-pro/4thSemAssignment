b6bf3bfb0d0159fe32de16d3720102859b9288dd
rohan37-pro <rohan2002maji@gmail.com>
2023-04-03 14:50:39 +0530

    palindrom strig assignment done

---
+++ 3_a_palindromString.py
@@ -0,0 +1,8 @@
+# wap to check given string palindrom or not
+
+s = input("enter a string : ")
+
+if s == s[-1::-1]:
+    print("yes palindrom string")
+else :
+    print("not palindrom syting")
---
+++ 3_b_wordCount.py
@@ -0,0 +1,1 @@
+# WAP that reads a line, then count words and display how many words are there in the line.
