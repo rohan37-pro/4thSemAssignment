233e08756cbc634955d9e4111f67ef699015241c
rohan37-pro <rohan2002maji@gmail.com>
2023-04-03 14:59:06 +0530

    substring count

---
+++ 3_c_subStringCount.py
@@ -0,0 +1,5 @@
+# WAP that reads a line, then counts how hamy times a substring appears in the line and display the count.
+
+S = input("enter string : ")
+sub = input("enter substring: ")
+print(f"Substring {sub} appears {S.count(sub)} times..")
